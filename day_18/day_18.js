c08c9e53fd04727ed0fb2c7c0896310a3f3ccd79
Satendra Rajput <satendracoder@gmail.com>
2026-01-22 07:08:09 +0530

    day_18

---
+++ day_18/day_18.js
@@ -0,0 +1,20 @@
+var validWordAbbreviation = function(word, abbr) {
+    let i = 0, j = 0;
+
+    while (i < word.length && j < abbr.length) {
+        if (isNaN(abbr[j])) {
+            if (word[i] !== abbr[j]) return false;
+            i++;
+            j++;
+        } else {
+            if (abbr[j] === '0') return false; // leading zero not allowed
+            let num = 0;
+            while (j < abbr.length && !isNaN(abbr[j])) {
+                num = num * 10 + Number(abbr[j]);
+                j++;
+            }
+            i += num;
+        }
+    }
+    return i === word.length && j === abbr.length;
+};
---
+++ day_18/readme.md
@@ -0,0 +1,17 @@
+# Day 20 – Valid Word Abbreviation
+
+Check if an abbreviation correctly represents a given word.
+
+Example:
+Input: word = "internationalization", abbr = "i12iz4n"  
+Output: true
+
+Approach:
+
+- Use two pointers
+- Parse numbers in abbreviation and skip characters in word
+
+Time Complexity: O(n)  
+Space Complexity: O(1)
+
+LeetCode: Valid Word Abbreviation (Easy)
