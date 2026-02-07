1b3ed997a288835312b2bfc78505aff5e02088b9
Satendra Rajput <114211179+satendracoder@users.noreply.github.com>
2026-02-07 23:12:45 +0530

    Create day_07.js

---
+++ february/day_07/day_07.js
@@ -0,0 +1,10 @@
+var maxSubArray = function(nums) {
+    let curr = nums[0];
+    let max = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        curr = Math.max(nums[i], curr + nums[i]);
+        max = Math.max(max, curr);
+    }
+    return max;
+};
