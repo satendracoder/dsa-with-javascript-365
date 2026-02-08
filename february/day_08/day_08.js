56045ff27f8d4dddb2b182fe66cef6253aa23970
Satendra Rajput <114211179+satendracoder@users.noreply.github.com>
2026-02-08 22:35:18 +0530

    Implement moveZeroes function to reorder array

---
+++ february/day_08/day_08.js
@@ -0,0 +1,15 @@
+var moveZeroes = function(nums) {
+    let index = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[index] = nums[i];
+            index++;
+        }
+    }
+
+    while (index < nums.length) {
+        nums[index] = 0;
+        index++;
+    }
+};
