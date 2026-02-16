66f5963030427610444eed9b637360950adcb2c2
Satendra Rajput <114211179+satendracoder@users.noreply.github.com>
2026-02-16 17:57:08 +0530

    Add palindrome checking function

---
+++ february/day_16/day_16.js
@@ -0,0 +1,23 @@
+var isPalindrome = function(s) {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    while (left < right && !/[a-z0-9]/i.test(s[left])) {
+      left++;
+    }
+
+    while (left < right && !/[a-z0-9]/i.test(s[right])) {
+      right--;
+    }
+
+    if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+      return false;
+    }
+
+    left++;
+    right--;
+  }
+
+  return true;
+};
