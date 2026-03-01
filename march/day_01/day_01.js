b2f26c03dd9807888e13c80d112d4a3b6dbd8a11
Satendra Rajput <114211179+satendracoder@users.noreply.github.com>
2026-03-01 17:56:15 +0530

    Implement happy number algorithm in day_01.js

---
+++ march/day_01/day_01.js
@@ -0,0 +1,22 @@
+var isHappy = function(n) {
+  let seen = new Set();
+
+  while (n !== 1 && !seen.has(n)) {
+    seen.add(n);
+    n = getNext(n);
+  }
+
+  return n === 1;
+};
+
+function getNext(num) {
+  let sum = 0;
+
+  while (num > 0) {
+    let digit = num % 10;
+    sum += digit * digit;
+    num = Math.floor(num / 10);
+  }
+
+  return sum;
+}
