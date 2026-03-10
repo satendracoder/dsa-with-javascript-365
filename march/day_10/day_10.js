64a12e4e17fcb1eaa79c829b55c3d697443e2160
Satendra Rajput <114211179+satendracoder@users.noreply.github.com>
2026-03-10 09:57:44 +0530

    Implement function to find longest consecutive sequence

---
+++ march/day_10/day_10.js
@@ -0,0 +1,20 @@
+var longestConsecutive = function(nums) {
+  const set = new Set(nums);
+  let longest = 0;
+
+  for (let num of set) {
+    if (!set.has(num - 1)) {
+      let current = num;
+      let length = 1;
+
+      while (set.has(current + 1)) {
+        current++;
+        length++;
+      }
+
+      longest = Math.max(longest, length);
+    }
+  }
+
+  return longest;
+};
